a2f09e777220d7f32dad947fc78fd3a1f8ed0c8e
Mark Morgan <momipsl@ipsl.jussieu.fr>
2015-07-28 17:34:35 +0200

    Bumperd version

--- pyesdoc/__init__.py
+++ pyesdoc/__init__.py
@@ -49,7 +49,7 @@ from pyesdoc import validation
 
 # Standard package info.
 __title__ = 'pyesdoc'
-__version__ = '0.9.2.0'
+__version__ = '0.9.2.1'
 __author__ = 'ES-DOC'
 __license__ = 'GPL'
 __copyright__ = 'Copyright 2015 ES-DOC'
