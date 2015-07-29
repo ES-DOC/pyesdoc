0b3bcd49ce2dd231e4275148bc834d930d4e4c42
Mark Morgan <momipsl@ipsl.jussieu.fr>
2015-07-29 11:32:18 +0200

    Extended config loader to load local pyesdoc.conf if found.

--- pyesdoc/utils/config.py
+++ pyesdoc/utils/config.py
@@ -40,22 +40,38 @@ def _init_from_shell_config():
         dir_path = os.path.dirname(dir_path)
 
 
+def _init_from_local_config():
+    """Attempts to initialize configuration from local file system.
+
+    """
+    # Find.
+    dir_path = os.path.dirname(os.path.abspath(__file__))
+    while dir_path != '/':
+        config_path = os.path.join(dir_path, "pyesdoc.conf")
+        if os.path.exists(config_path):
+            return config_path
+        dir_path = os.path.dirname(dir_path)
+
+
 def _init_from_user_home():
     """Attempts to initialize configuration from a file foud on user's home directory.
 
     """
-    config_path = os.path.join(_HOME, ".esdoc")
+    config_path = os.path.join(_HOME, ".pyesdoc")
     if os.path.exists(config_path):
         return config_path
 
 
 def _init():
-    """Initializes configuration."""
+    """Initializes configuration.
+
+    """
     global data
 
     # Attempt to load config from file system.
     for func in (
         _init_from_shell_config,
+        _init_from_local_config,
         _init_from_user_home,
         ):
         config_path = func()
@@ -72,12 +88,14 @@ def _init():
             WARNING :: pyesdoc configuration file not found.
             If you are running pyesdoc as part of the ES-DOC shell then the config file location should be:
                 SHELL-HOME/ops/config/pyesdoc.conf.
+            If you are running pyesdoc as part of the ES-DOC notebook-shell then the config file location should be:
+                SHELL-HOME/pyesdoc.conf.
             If you are running pyesdoc in standalone mode then the config file location should be:
-                $HOME/.esdoc
+                $HOME/.pyesdoc
             If neither of these files exist then:
                 1.  Download the following default configuration file:
                     https://raw.githubusercontent.com/ES-DOC/esdoc-shell/master/resources/user/template-pyesdoc.conf
-                2.  Copy downloaded file to $HOME/.esdoc
+                2.  Copy downloaded file to $HOME/.pyesdoc
             If this problem persists them contact es-doc-support@list.woc.noaa.gov
         """
         raise RuntimeError(msg)
