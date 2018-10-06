2dbe50a63dc2eca6ba0416dc33b47c43f44ee65e
Mark Greenslade <momipsl@ipsl.jussieu.fr>
2018-10-06 13:10:09 +0200

    1. Added new extender for cim 2 model.

--- pyesdoc/_extensions/cim/v2/__init__.py
+++ pyesdoc/_extensions/cim/v2/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from pyesdoc._extensions.cim.v2 import designing_numerical_experiment
 from pyesdoc._extensions.cim.v2 import designing_project
+from pyesdoc._extensions.cim.v2 import science_model
 
 
 
@@ -8,4 +9,5 @@ from pyesdoc._extensions.cim.v2 import designing_project
 SUPPORTED = {
 	"cim.2.designing.numericalexperiment": designing_numerical_experiment,
 	"cim.2.designing.project": designing_project,
+	"cim.2.science.model": science_model
 }
---
+++ pyesdoc/_extensions/cim/v2/science_model.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+"""
+.. module:: designing_numerical_experiment.py
+   :platform: Unix, Windows
+   :synopsis: Extends a cim.v1.designing.numerical_experiment document.
+
+.. moduleauthor:: Mark Conway-Greenslade <momipsl@ipsl.jussieu.fr>
+
+
+"""
+from pyesdoc import constants
+
+
+
+def get_extenders():
+    """Returns set of extension functions.
+
+    """
+    return (
+        _set_summary_fields,
+        )
+
+
+def _set_summary_fields(ctx):
+    """Sets related experiment viewer urls.
+
+    """
+    ctx.ext.summary_fields = (
+        ctx.doc.canonical_id.upper(),
+        ctx.doc.long_name
+    )
