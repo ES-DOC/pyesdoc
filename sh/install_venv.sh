153c778158470e99b9247958480e80efe9219571
Mark A. Greenslade <mark@casperlabs.io>
2020-12-20 20:20:26 +0100

    1. Updated installer.

--- sh/install_venv.sh
+++ sh/install_venv.sh
@@ -8,13 +8,14 @@ main()
 {
     log "installing virtual environment ..."
 
+    pushd $PYESDOC_HOME
+
+    # Update pip / pipenv to latest versions.
     pip install --upgrade pip
-    pip install --upgrade virtualenv
-    virtualenv $PYESDOC_HOME/ops/venv
-    source $PYESDOC_HOME/sh/activate_venv.sh
-    pip install --upgrade pip
-    pip install --upgrade --no-cache-dir -I -r $PYESDOC_HOME/requirements.txt
-    deactivate
+    pip install --upgrade pipenv
+
+	# Install venv using pip env.
+	pipenv install
 }
 
 # Invoke entry point.
